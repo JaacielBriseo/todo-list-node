dd3024e6ed24e1f44d47dd39a59f0319f540d5bc
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-19 19:40:39 -0700

    stdin, stdout , readline

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('colors');
-
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async () => {
 	console.log('Hola mundo');
+	mostrarMenu();
+	// pausa();
 };
 main();
