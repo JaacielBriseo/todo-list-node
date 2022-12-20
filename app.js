f781403101cc71c57eec69bd1c0cbb2674035504
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-19 19:49:34 -0700

    do-while loop for menu

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async () => {
 	console.log('Hola mundo');
-	mostrarMenu();
+	let opt = '';
+	do {
+		opt = await mostrarMenu();
+        console.log({opt});
+		if(opt !== '0') await pausa();
+	} while (opt !== '0');
 	// pausa();
 };
 main();
